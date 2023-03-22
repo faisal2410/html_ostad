f6225bf63b6167ed93ce4771e4cde8a85b572e47
Faisal ahmed <faisal2410@yahoo.com>
2023-03-22 19:55:27 +0600

    initial commit

---
+++ steps.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Start with the basics: Learn the basic syntax of HTML, which includes the basic structure of an HTML document, how to create headings, paragraphs, and lists.
+
+Learn the different HTML tags: There are a lot of HTML tags available, and it's important to learn how to use them. Some common tags include links, images, tables, and forms.
+
+Practice: The best way to learn HTML is by practicing what you've learned. Try creating simple web pages and experiment with different tags.
+
+Learn CSS: CSS is used to style HTML pages, so it's important to learn how to use it. Start with basic styling techniques like changing the color and font of text.
+
+Responsive Design: Learn how to make your web pages responsive, so they can adapt to different screen sizes and devices.
+
+Accessibility: Learn how to make your web pages accessible to users with disabilities. This includes using proper HTML tags, providing alt text for images, and making sure your pages are keyboard accessible.
+
+Learn HTML5 and CSS3: HTML5 and CSS3 are the latest versions of HTML and CSS, respectively. They introduce new features like video and audio support and new styling options.
+
+Learn JavaScript: JavaScript is a programming language that can be used to add interactivity to web pages. Learn how to use JavaScript to create dynamic web pages.
+
+Practice, Practice, Practice: Keep practicing your HTML skills by creating more complex web pages and experimenting with new techniques.
+
+Stay up-to-date: HTML is always evolving, so it's important to stay up-to-date with the latest standards and best practices.
+
+Remember, learning HTML takes time and practice. But with persistence, you can become proficient in creating amazing web pages!
+*/ 
